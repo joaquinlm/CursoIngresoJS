38469cec054bf81fae846368bd9c837b4f5b87f7
joaquinlm <nodomadera@gmail.com>
2021-07-05 20:30:44 -0300

    primer commit

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,8 +1,9 @@
-/*
+/*Lubo Millán, Joaquín | División H
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
-{
-	alert("ok");
+{	
+	let nombre = prompt("Decime tu nombre");
+	alert(nombre);
 }
 
