c24ec45588fd98eb9249b9009d4cf5e67b72c6b3
joaquinlm <nodomadera@gmail.com>
2021-07-05 21:19:37 -0300

    segundo commit

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -1,9 +1,10 @@
-/* 
+/* Lubo Millán, Joaquín | División H
+Ejercicio E/S 4
 Debemos lograr tomar un dato por 'PROMPT' 
 y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
-function mostrar()
-{
-	alert("ok");
+function mostrar() {
+  let nombre = prompt('Cuál es tu nombre?');
+  let cajaDeTexto = document.getElementById('txtIdNombre');
+  cajaDeTexto.value = 'Tu nombre es: ' + nombre;
 }
-
